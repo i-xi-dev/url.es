f5dbabfd0c2af44bcd944cdfe38f20b3645d54e3
i-xi-dev <naoyuki.isono@gmail.com>
2023-09-14 23:50:29 +0900

    3.1.8

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { NonNegativeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.0/mod.ts";
-export { Percent } from "https://raw.githubusercontent.com/i-xi-dev/percent.es/4.0.23/mod.ts";
+export { NonNegativeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.1/mod.ts";
+export { Percent } from "https://raw.githubusercontent.com/i-xi-dev/percent.es/4.0.24/mod.ts";
