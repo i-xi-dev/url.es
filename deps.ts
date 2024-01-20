c177ac0216babfc5d92897c298a57d4229af9f77
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-20 11:54:18 +0900

    3.1.17

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export { SafeInteger } from "https://raw.githubusercontent.com/i-xi-dev/number.es/6.0.0/mod.ts";
-export { Percent } from "https://raw.githubusercontent.com/i-xi-dev/percent.es/4.0.31/mod.ts";
+export { Percent } from "https://raw.githubusercontent.com/i-xi-dev/percent.es/4.0.32/mod.ts";
